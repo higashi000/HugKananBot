c0808aca92c0aafc14deb80efadc9cc0d152e0c9
higashi000 <higashi.ict@gmail.com>
2019-02-25 14:28:09 +0900

    [change] already searched tweet

--- source/main.rb
+++ source/main.rb
@@ -8,42 +8,67 @@ require 'twitter'
 end
 
 
-$finalReplyID
-$canFirst = true
 def timeLine
-  overlapReply = false
-  kananSerif = ["ハグしよ！", "ダイブいい感じ！", "ご機嫌いかがかなん？", "8!", "訴えるよ", "何か心配事なら, 相談に乗るよ", "一緒に潜ってみる？", "ん？私ならここにいるよ", "あっはは。結構甘えん坊なんだね？　千歌に似てるかも♪", "焦らずいこう♪"]
-  @client.home_timeline.each do |tweet|
-    nowTime = Time.now
+  kananSerif = ["ハグしよ！", "ダイブいい感じ！", "ご機嫌いかがかなん？", "8!", "訴えるよ",
+                "何か心配事なら, 相談に乗るよ", "一緒に潜ってみる？", "ん？私ならここにいるよ",
+                "あっはは。結構甘えん坊なんだね？　千歌に似てるかも♪", "焦らずいこう♪"]
 
-    if $finalReplyID == tweet.id then
-      overlapReply = true
+  @client.home_timeline.each do |tweet|
+    time = TIme.new
+    nowTime = time.year.to_s
+    tweetTime = getTweetTime(tweet.id).strftime("%Y%m%d%H%M").to_i
+
+    if countDigit(time.mon) == 1 then
+      nowTime += "0" + time.mon.to_s
+    else
+      nowTime += time.mon.to_s
     end
 
-    if tweet.text == "@Kanan136_bot ハグしよ！" && !overlapReply then
-      if $canFirst then
-        $finalReplyID = tweet.id
-        $canFirst = false
-      end
+    if countDigit(time.day) == 1 then
+      nowTime += "0" + time.day.to_s
+    else
+      nowTime += time.day.to_s
+    end
 
+    if countDigit(time.hour) == 1 then
+      nowTime += "0" + time.hour.to_s
+    else
+      nowTime += time.hour.to_s
+    end
+
+    if countDigit(time.min) == 1 then
+      nowTime += "0" + time.min.to_s
+    else
+      nowTime += time.min.to_s
+    end
+
+    nowTime = nowTime.to_i
+
+    if tweet.text == "@Kanan136_bot ハグしよ！" && tweetTime > (nowTime - 2) then
       @client.update("@#{tweet.user.screen_name} #{kananSerif[rand(0 .. 9)]}", options = {:in_reply_to_status_id => tweet.id})
     end
   end
+end
+
+def getTweetTime(id)
+  return TIme.at(((id.to_i >> 22) + 1288834974657) / 1000.0)
+end
+
+def countDigit(num)
+  return num.to_s.length
 end
 
 tweetFlg = true
 
 loop do
   time = Time.new
-  canTweet = time.sec
 
-  if canTweet == 0 && tweetFlg then
+  if time.sec == 0 && tweetFlg then
     timeLine
 
-    $canFirst = true
     tweetFlg = false
   end
-  if canTweet != 0 then
+  if time.sec != 0 then
     tweetFlg = true
   end
 end
