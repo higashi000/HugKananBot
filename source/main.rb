939e9931c886f9d3fcc44ec68dc82169bc1c27e3
higashi000 <higashi.ict@gmail.com>
2019-02-26 23:04:20 +0900

    [change] Search from TL to word search

--- source/main.rb
+++ source/main.rb
@@ -13,10 +13,12 @@ def timeLine
                 "何か心配事なら, 相談に乗るよ", "一緒に潜ってみる？", "ん？私ならここにいるよ",
                 "あっはは。結構甘えん坊なんだね？　千歌に似てるかも♪", "焦らずいこう♪"]
 
-  @client.home_timeline.each do |tweet|
+  searchString = "to:Kanan136_bot"
+
+  @client.search(searchString, :count => 100, :result_type => "recent").map do |tweet|
     time = Time.new
     nowTime = time.year.to_s
-    tweettime = tweet_id2time(tweet.id).strftime("%Y%m%d%H%M").to_i
+    tweettime = getTweetTime(tweet.id).strftime("%Y%m%d%H%M").to_i
 
     if countDigit(time.mon) == 1 then
       nowTime += "0" + time.mon.to_s
@@ -44,21 +46,25 @@ def timeLine
 
     nowTime = nowTime.to_i
 
+    if tweettime < (nowTime - 2) then
+      break
+    end
+
     p tweet.text
     p tweettime
     p nowTime
 
-    if tweet.text == "@Kanan136_bot ぽよ" && tweettime > (nowTime - 2) then
+    if tweet.text == "@Kanan136_bot ぽよ" && tweettime >= (nowTime - 2) then
       @client.update("@#{tweet.user.screen_name} #{kananSerif[rand(0 .. 9)]}", options = {:in_reply_to_status_id => tweet.id})
     end
 
-    if tweet.text == "@Kanan136_bot ぽよよ" && tweettime > (nowTime - 2) then
+    if tweet.text == "@Kanan136_bot ぽよよ" && tweettime >= (nowTime - 2) then
       @client.update("@#{tweet.user.screen_name} #{kananSerif[rand(0 .. 9)]}", options = {:in_reply_to_status_id => tweet.id})
     end
   end
 end
 
-def tweet_id2time(id)
+def getTweetTime(id)
   return Time.at(((id.to_i >> 22) + 1288834974657) / 1000.0)
 end
 
