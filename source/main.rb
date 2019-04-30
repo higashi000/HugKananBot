211e1ce3e28c52528ad326a2798f0d9a63e870c4
higashi000 <higashi.ict@gmail.com>
2019-04-30 22:19:23 +0900

    [add] slot func

--- source/main.rb
+++ source/main.rb
@@ -1,5 +1,4 @@
 require 'twitter'
-require './slot'
 
 @client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['CONSUMER_KEY']
@@ -107,6 +106,60 @@ def getSlot(tweetText)
   end
 
   return false
+end
+
+def musicSlot
+  music = ["君の心は輝いてるかい？", "Step! ZERO to ONE", "Aqours☆HEROES",
+           "恋になりたいAQOUARIUM", "待ってて愛の歌", "届かない星だとしても",
+           "青空Jumping Heart", "ハミングフレンド",
+           "ユメ語るよりユメ歌おう", "サンシャインぴっかぴか温度",
+           "夢で夜空を照らしたい", "未熟DREAMER",
+           "Pops heartで踊るんだもん！",
+           "想いよひとつになれ", "MIRAI TICKET",
+           "ジングルベルがとまらない", "聖なる日の祈り",
+           "Daydream warrior",
+           "スリリング・ワンウェイ",
+           "太陽を追いかけろ!",
+           "HAPPY PARTY TRAIN", "SKY JOURNEY", "少女以上の恋がしたい",
+           "Landing action Yeah!!",
+           "未来の僕らは知ってるよ", "君の瞳をめぐる冒険",
+           "勇気はどこに？君の胸に！", "\"MY LIST\" to you",
+           "MY舞☆TONIGHT", "MIRACLE WAVE",
+           "WATER BLUE NEW WORLD", "WONDERFUL STORIES",
+           "キセキヒカル",
+           "ホップ・ステップ・ワーイ！",
+           "Thank you, FRIENDS!!", "No,10",
+           "僕らの走ってきた道は・・・", "Next SPARKLING!!",
+           "Hop?Stop?Nonstop!",
+           "Brightest Melody",
+           "Awaken the power", "Over The Next Rainbow",
+           "元気全開DAY!DAY!DAY!", "夜空はなんでも知ってるの？",
+           "P.S.の向こう側",
+           "近未来ハッピーエンド", "海岸通りで待ってるよ",
+           "サクラバイバイ",
+           "トリコリコPLEASE!!", "ときめき分類学",
+           "LONELY TUNING",
+           "GALAXY HidE and SeeK", "INNOCENT BIRD",
+           "卒業ですね",
+           "Strawberry Trapper", "Guilty Night, Guilty Kiss!",
+           "Guilty Eyes Fever",
+           "コワレヤスキ", "Shadow gate to love",
+           "Guilty!? Farewell Party",
+           "SELF CONTROL!!", "CLASH MIND", "DROPOUT!?", "Believe again",
+           "Waku-Waku-Week!", "ハジマリロード",
+           "決めたよHand in Hand", "大好きだったらダイジョブ",
+           "空も心も晴れるから", "Marine Border Parasol",
+           "G線上のシンデレラ", "予測不可能Driving!", "逃走迷走メビウスループ",
+           "夏への扉 Never end ver.", "真夏は誰のモノ？",
+           "地元愛♡満タン☆サマーライフ", "夏の終わりの雨音が",
+           "One More Sunshine Story", "おやすみなさん！", "in this unstable world",
+           "Pianoforte Monologue", "Beginner's Sailing", "RED GEM WINK",
+           "WHITE FIRST LOVE", "さかなかなんだか", "New winding road"
+  ]
+
+
+  p music.length
+  return misic[rand(0 .. music.length)]
 end
 
 def getTweetTime(id)
